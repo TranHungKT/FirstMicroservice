5d9da2172f8eae15e3984f1697ac4bcdc79e7155
Hung_Tran <tranhung26122612@gmail.com>
2021-05-06 17:16:27 +0700

    move image of speaker to speaker service

--- provided/conference-app/server/routes/index.js
+++ provided/conference-app/server/routes/index.js
@@ -1,33 +1,45 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const speakersRoute = require('./speakers');
-const feedbackRoute = require('./feedback');
+const speakersRoute = require("./speakers");
+const feedbackRoute = require("./feedback");
 
 module.exports = (param) => {
-  const { speakers } = param;
+    const { speakers } = param;
 
-  router.get('/', async (req, res, next) => {
-    try {
-      const promises = [];
-      promises.push(speakers.getListShort());
-      promises.push(speakers.getAllArtwork());
+    router.get("/images/:type/:file", async (req, res) => {
+        try {
+            const image = await speakers.getImage(
+                `${req.params.type}/${req.params.file}`
+            );
 
-      const results = await Promise.all(promises);
+            return image.pipe(res);
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
-      return res.render('index', {
-        page: 'Home',
-        speakerslist: results[0],
-        artwork: results[1],
-      });
-    } catch (err) {
-      return next(err);
-    }
-  });
+    router.get("/", async (req, res, next) => {
+        try {
+            const promises = [];
+            promises.push(speakers.getListShort());
+            promises.push(speakers.getAllArtwork());
 
-  router.use('/speakers', speakersRoute(param));
-  router.use('/feedback', feedbackRoute(param));
+            const results = await Promise.all(promises);
 
-  return router;
+            return res.render("index", {
+                page: "Home",
+                speakerslist: results[0],
+                artwork: results[1]
+            });
+        } catch (err) {
+            return next(err);
+        }
+    });
+
+    router.use("/speakers", speakersRoute(param));
+    router.use("/feedback", feedbackRoute(param));
+
+    return router;
 };
